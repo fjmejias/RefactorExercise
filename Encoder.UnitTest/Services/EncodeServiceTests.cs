6fede3c09959fbe21f5272d7195027329015513f
fjma <fjma@kamstrup.com>
2022-03-20 22:14:35 +0100

    create a new test to encode + decode a text

--- Encoder.UnitTest/Services/EncodeServiceTests.cs
+++ Encoder.UnitTest/Services/EncodeServiceTests.cs
@@ -18,26 +18,39 @@ namespace Encoder.UnitTest.Services
         public void Given_InputText_When_Encode_Then_Expected_Encoded_Text()
         {
             // given
-            var inputText = "This is an input text";
+            var inputText = "This is an input test string";
 
             // when
             var result = _sut.Encode(inputText);
 
             // then
-            Assert.AreEqual("VGhpcyBpcyBhbiBpbnB1dCB0ZXh0", result);
+            Assert.AreEqual("VGhpcyBpcyBhbiBpbnB1dCB0ZXN0IHN0cmluZw==", result);
         }
 
         [Test]
         public void Given_EncodedText_When_Decode_Then_Expected_Decoded_Text()
         {
             // given
-            var encodedText = "VGhpcyBpcyBhbiBpbnB1dCB0ZXh0";
+            var encodedText = "VGhpcyBpcyBhbiBpbnB1dCB0ZXN0IHN0cmluZw==";
 
             // when
             var result = _sut.Decode(encodedText);
 
             // then
-            Assert.AreEqual("This is an input text", result);
+            Assert.AreEqual("This is an input test string", result);
+        }
+
+        [Test]
+        public void Given_InputText_When_Encode_And_Decode_Then_Expected_InputText()
+        {
+            // given
+            var inputText = "This is an input test string";
+
+            // when
+            var result = _sut.Decode(_sut.Encode(inputText));
+
+            // then
+            Assert.AreEqual(inputText, result);
         }
     }
 }
