43fef3c3e44d270bd148234674a97e620db54fb2
fjma <fjma@kamstrup.com>
2022-03-20 22:32:40 +0100

    added new project for Question2

---
+++ ConsoleApp1/HighCard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Question2
+{
+    public class HighCard
+    {
+        public HighCard()
+        {
+        }
+
+        public bool Play()
+        {
+            Random rnd = new Random();
+
+            int i = rnd.Next() % 52 + 1;
+            int j = rnd.Next() % 52 + 1;
+
+            return i < j;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Question2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HighCard card = new HighCard();
+            Console.WriteLine(card.Play() ? "win" : "lose");
+        }
+    }
+}
