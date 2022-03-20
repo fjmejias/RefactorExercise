80551f42c5ae50ff3727dc4ba9242e4cb5af891d
fjma <fjma@kamstrup.com>
2022-03-20 22:14:16 +0100

    fix the Decode service to remove Equals characters to fix the decoded text

--- Encoder/Services/EncodeService.cs
+++ Encoder/Services/EncodeService.cs
@@ -1,11 +1,13 @@
 ﻿using Encoder.Interfaces;
 using System;
+using System.Linq;
 
 namespace Encoder.Services
 {
     public class EncodeService : IEncodeService
     {
         private readonly char[] _transCode;
+        private const char EqualChar = '=';
 
         public EncodeService()
         {
@@ -13,10 +15,10 @@ namespace Encoder.Services
         }
 
         private char[] InitTransCode()
-        {                    
+        {
             var transCode = new char[65];
-            for (int i = 0; i < 64; i++)  
-            {    
+            for (int i = 0; i < 64; i++)
+            {
                 transCode[i] = (char)('A' + i);
                 if (i > 25)
                     transCode[i] = (char)(transCode[i] + 6);
@@ -37,26 +39,26 @@ namespace Encoder.Services
             int cb = (l / 3 + (Convert.ToBoolean(l % 3) ? 1 : 0)) * 4;
 
             char[] output = new char[cb];
-            for ( int i = 0; i < cb; i++ )
+            for (int i = 0; i < cb; i++)
             {
-                output[i] = '=';
+                output[i] = EqualChar;
             }
- 
+
             int c = 0;
             int reflex = 0;
-            const int s = 0x3f;             
- 
+            const int s = 0x3f;
+
             for (int j = 0; j < l; j++)
             {
                 reflex <<= 8;
-                reflex &= 0x00ffff00;         
+                reflex &= 0x00ffff00;
                 reflex += input[j];
- 
-                int x = (j%3+1)*2;          
+
+                int x = (j % 3 + 1) * 2;
                 int mask = s << x;
                 while (mask >= s)
                 {
-                    int pivot =  (reflex & mask) >> x;
+                    int pivot = (reflex & mask) >> x;
                     output[c++] = _transCode[pivot];
                     int invert = ~mask;
                     reflex &= invert;
@@ -64,8 +66,8 @@ namespace Encoder.Services
                     x -= 6;
                 }
             }
- 
-            switch (l%3)
+
+            switch (l % 3)
             {
                 case 1:
                     reflex <<= 4;
@@ -83,27 +85,27 @@ namespace Encoder.Services
         public string Decode(string input)
         {
             int l = input.Length;
-            int cb = (l/4 + (Convert.ToBoolean(l%4)?1:0))*3+1;   
-            char[] output = new char[cb];        
+            int numEquals = input.Count(s => s == EqualChar);
+            int cb = (l / 4 + (Convert.ToBoolean(l % 4) ? 1 : 0)) * 3 - numEquals;
+            char[] output = new char[cb];
             int c = 0;
             int bits = 0;
             int reflex = 0;
-            bool fTerminate = false;
-            for (int j = 0; j < l && !fTerminate; j++)
+            for (int j = 0; j < l && input[j] != EqualChar; j++)
             {
                 reflex <<= 6;
                 bits += 6;
-                fTerminate = '=' == input[j];
-                if (!fTerminate)
-                    reflex += Array.IndexOf(_transCode, input[j]);
-    
-                while (bits >= 8)
+                if (input[j] != EqualChar)
                 {
-                    int mask = 0x000000ff << (bits % 8);                                        
-                    output[c++] = (char)((reflex & mask) >> (bits % 8));    
-                    int invert = ~mask;
-                    reflex &= invert;
-                    bits -= 8;
+                    reflex += Array.IndexOf(_transCode, input[j]);
+                    while (bits >= 8)
+                    {
+                        int mask = 0x000000ff << (bits % 8);
+                        output[c++] = (char)((reflex & mask) >> (bits % 8));
+                        int invert = ~mask;
+                        reflex &= invert;
+                        bits -= 8;
+                    }
                 }
             }
 
