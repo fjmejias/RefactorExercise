c58f8b7574623d3a59fe713db5c41fba9e959c19
fjma <fjma@kamstrup.com>
2022-03-21 18:35:42 +0100

    create a new unit test project for the HighCard class

---
+++ HighCard.UnitTest/HighCardTests.cs
@@ -0,0 +1,20 @@
+﻿using HighCard.Interfaces;
+using Moq;
+using NUnit.Framework;
+
+namespace HighCard.UnitTest
+{
+    public class HighCardTests
+    {
+        private IHighCard _sut;
+        private Mock<IRandomGenerator> _randomGeneratorMock;
+
+        [SetUp]
+        public void SetupTests()
+        {
+            _randomGeneratorMock = new Mock<IRandomGenerator>();
+            
+            _sut = new HighCard(_randomGeneratorMock.Object);
+        }
+    }
+}
