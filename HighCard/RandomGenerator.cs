8176cb317ce7e4d4c3719e3f472a20aa00013916
fjma <fjma@kamstrup.com>
2022-03-21 18:26:55 +0100

    add an implementation of the IRandomGenerator interfaces that uses the Random class to generate numbers

---
+++ HighCard/RandomGenerator.cs
@@ -0,0 +1,20 @@
+﻿using HighCard.Interfaces;
+using System;
+
+namespace HighCard
+{
+    public class RandomGenerator : IRandomGenerator
+    {
+        private readonly Random _rnd;
+
+        public RandomGenerator()
+        {
+            _rnd = new Random(DateTime.Now.Millisecond);
+        }
+
+        public int Next()
+        {
+            return _rnd.Next();
+        }
+    }
+}
