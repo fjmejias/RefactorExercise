640dad0a137c03998828472c3a7e849ae7dfbe4c
fjma <fjma@kamstrup.com>
2022-03-22 19:12:13 +0100

    refactor the Program.cs to register now the new settings as a Instance and print more information about the game

--- Question2.ConsoleApp/Program.cs
+++ Question2.ConsoleApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using HighCard;
+using HighCard.Configuration;
 using HighCard.Interfaces;
+using HighCard.Models;
 using System;
 using Unity;
 
@@ -13,19 +15,56 @@ namespace Question2.ConsoleApp
 
             var game = highCardGame.Play();
             Console.WriteLine($"HIGHCARD Game - Date: {game.Date}");
-            Console.WriteLine($"Player 1: {game.PlayerA.Name} - Card: {game.PlayerA.PlayingCard.Number}");
-            Console.WriteLine($"Player 2: {game.PlayerB.Name} - Card: {game.PlayerB.PlayingCard.Number}");
-            Console.WriteLine($"{game.GameResult} - {(game.PlayerA.Winner ? game.PlayerA.Name : game.PlayerB.Name)}");
+            Console.WriteLine(PrintPlayerCard(game.PlayerA));
+            Console.WriteLine(PrintPlayerCard(game.PlayerB));
+            Console.WriteLine(PrintGameResult(game));
         }
 
         private static IUnityContainer RegisterHighCard()
         {
             var container = new UnityContainer();
 
-            container.RegisterType<IRandomGenerator, RandomGenerator>();
+            var settings = new HighCardSettings
+            {
+                NumCardsPerSuit = 13,
+                NumDecks = 3
+            };
+
+            container.RegisterInstance(settings);
+            container.RegisterType<ICardSelector, CardSelector>();
             container.RegisterType<IHighCard, HighCard.HighCard>();
 
             return container;
+        }
+
+        private static string PrintPlayerCard(Player player)
+        {
+            return$"{player.Name} - Card: {player.PlayingCard.Number} of {player.PlayingCard.Suit}";
+        }
+
+        private static string PrintGameResult(Game game)
+        {
+            var result = $"{game.GameResult} - {(game.PlayerA.Winner ? game.PlayerA.Name : game.PlayerB.Name)}";
+
+            switch (game.GameResult)
+            {
+                case HighCard.Enums.GameResult.PlayerWins:
+                    result = $"The winner is: {(game.PlayerA.Winner ? game.PlayerA.Name : game.PlayerB.Name)}";
+                    break;
+                case HighCard.Enums.GameResult.Tie:
+                    result = "Tied game";
+                    break;
+                case HighCard.Enums.GameResult.Error:
+                    result = "There was an error";
+                    break;
+            }
+
+            if (game.GameResult == HighCard.Enums.GameResult.PlayerWins)
+            {
+                
+            }
+
+            return result;
         }
     }
 }
